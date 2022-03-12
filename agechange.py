51d0c76ccc27f7dd73c6b1596c5f2247ff3562eb
Samuel Wang <89223557+Samuelwaang@users.noreply.github.com>
2022-03-12 23:48:06 +0000

    Menu and animation code

---
+++ agechange.py
@@ -0,0 +1,9 @@
+def ageChange(age1, age2):
+    #print(age1, age2)
+    if (age1 > age2):
+        num = age1
+        age1 = age2
+        age2 = num
+    return age1,age2
+print( ageChange(16, 21) )
+print ( ageChange(21, 16) )
