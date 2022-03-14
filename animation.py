7fe49301e1a01f779eaed19875535bbf0bf387cc
Samuel Wang <89223557+Samuelwaang@users.noreply.github.com>
2022-03-14 18:57:37 +0000

    small edits

--- animation.py
+++ animation.py
@@ -1,21 +1,10 @@
 import time
 
-# terminal print commands
 ANSI_CLEAR_SCREEN = u"\u001B[2J"
 ANSI_HOME_CURSOR = u"\u001B[0;0H\u001B[2"
-OCEAN_COLOR = u"\u001B[44m\u001B[2D"
-SHIP_COLOR = u"\u001B[32m\u001B[2D"
 RESET_COLOR = u"\u001B[0m\u001B[2D"
 
-def ocean_print():
-    # print ocean
-    print(ANSI_CLEAR_SCREEN, ANSI_HOME_CURSOR)
-    print("\n\n\n\n")
-    print(OCEAN_COLOR + "  " * 35)
-
-
-# print ship with colors and leading spaces
-def ship_print(position):
+def face_print(position):
     print(ANSI_HOME_CURSOR)
     print(RESET_COLOR)
     sp = " " * position
@@ -27,10 +16,7 @@ def ship_print(position):
     print(RESET_COLOR)
 
 
-# ship function, iterface into this file
-def ship():
-    # only need to print ocean once
-    ocean_print()
+def face():
 
     # loop control variables
     start = 0  # start at zero
@@ -39,5 +25,5 @@ def ship():
 
     # loop purpose is to animate ship sailing
     for position in range(start, distance, step):
-        ship_print(position)  # call to function with parameter
+        face_print(position)  # call to function with parameter
         time.sleep(.1)
--- main.py
+++ main.py
@@ -1,30 +1,21 @@
-
 import animation
 
 main_menu = [
     ["menuOption1", None],
 ]
 
-# Submenu list of [Prompt, Action]
-# Works similarly to main_menu
 sub_menu = [
     ["Age Change", "agechange.py"],
 ]
 
 patterns_sub_menu = [
     ["Keypad", "keypad.py"],
-    ["Animation", animation.ship],
+    ["Animation", animation.face],
 ]
 
-# Menu banner is typically defined by menu owner
 border = "=" * 25
 banner = f"\n{border}\nPlease Select An Option\n{border}"
 
-
-# def menu
-# using main_menu list:
-# 1. main menu and submenu reference are created [Prompts, Actions]
-# 2. menu_list is sent as parameter to menuy.menu function that has logic for menu control
 def menu():
     title = "Function Menu" + banner
     menu_list = main_menu.copy()
@@ -32,9 +23,6 @@ def menu():
     menu_list.append(["Patterns", patterns_submenu])
     buildMenu(title, menu_list)
 
-# def submenu
-# using sub menu list above:
-# sub_menu works similarly to menu()
 def submenu():
     title = "Function Submenu" + banner
     buildMenu(title, sub_menu)
@@ -43,52 +31,37 @@ def patterns_submenu():
     buildMenu(title, patterns_sub_menu)
 
 def buildMenu(banner, options):
-    # header for menu
     print(banner)
-    # build a dictionary from options
     prompts = {0: ["Exit", None]}
     for op in options:
         index = len(prompts)
         prompts[index] = op
 
-    # print menu or dictionary
     for key, value in prompts.items():
         print(key, '->', value[0])
 
-    # get user choice
     choice = input("Type your choice> ")
 
-    # validate choice and run
-    # execute selection
-    # convert to number
     try:
         choice = int(choice)
         if choice == 0:
-            # stop
             return
         try:
-            # try as function
             action = prompts.get(choice)[1]
             action()
         except TypeError:
-            try:  # try as playground style
+            try:
                 exec(open(action).read())
             except FileNotFoundError:
                 print(f"File not found!: {action}")
-            # end function try
-        # end prompts try
     except ValueError:
-        # not a number error
         print(f"Not a number: {choice}")
     except UnboundLocalError:
-        # traps all other errors
         print(f"Invalid choice: {choice}")
     except TypeError:
         print(f"Not callable {action}")
-    # end validation try
 
-    buildMenu(banner, options)  # recursion, start menu over again
-
+    buildMenu(banner, options)
 
 if __name__ == "__main__":
     menu()
